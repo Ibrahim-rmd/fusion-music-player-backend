3bbada44583b0c331c1b16199e200cc23ca094f2
Ibrahim <ibrahim.zod@gmail.com>
2023-12-23 23:40:27 +0530

    changes

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.use("/api/songs", songsRoute);
 app.use("/api/admin", adminRoute);
 const port = process.env.PORT || 5000;
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-//   });
-// }
+
 
 app.listen(port, () => console.log(`Node js server started at port ${port}!`));
